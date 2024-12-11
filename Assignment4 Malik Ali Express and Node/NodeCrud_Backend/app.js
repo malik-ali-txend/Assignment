29c3cd37d1dafd9e61987e18bbf487b3f790e4d5
malik-ali-txend <malik.ali@txend.com>
2024-12-11 15:40:09 +0500

    upadted

--- Assignment4 Malik Ali Express and Node/NodeCrud_Backend/app.js
+++ Assignment4 Malik Ali Express and Node/NodeCrud_Backend/app.js
@@ -1,10 +1,22 @@
-// app.js
 const express = require('express');
 const path = require('path');
-const resourceController = require('./resourceController');  // Importing the controller
-
+const fs = require('fs');
 const app = express();
 const port = 3000;
+
+
+
+const readData = () => {
+  const dataPath = path.join(__dirname, 'data', 'resources.json');
+  const rawData = fs.readFileSync(dataPath, 'utf-8');
+  return JSON.parse(rawData);
+};
+
+const saveData = (data) => {
+  const dataPath = path.join(__dirname, 'data', 'resources.json');
+  fs.writeFileSync(dataPath, JSON.stringify(data, null, 2));
+};
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));  
@@ -14,17 +26,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.get('/', resourceController.getAllResources);
-app.get('/create', (req, res) => res.render('create'));
-app.get('/update/:id', resourceController.getResource);
-app.get('/delete/:id', resourceController.getResource);
+app.get('/create', (req, res) => {
+  res.render('create');  
+});
 
-app.post('/create', resourceController.createResource);
-app.post('/update/:id', resourceController.updateResource);
-app.post('/delete/:id', resourceController.deleteResource);
 
-// Start the server
+app.get('/', (req, res) => {
+  const resources = readData();  
+  res.render('index', { resources });  
+});
+
+
+app.get('/update/:id', (req, res) => {
+  const resourceId = req.params.id;
+  const resources = readData();
+  const resource = resources.find(r => r.id == resourceId);
+  res.render('update', { resource });
+});
+
+app.get('/delete/:id', (req, res) => {
+  const resourceId = req.params.id;
+  const resources = readData();
+  const resource = resources.find(r => r.id == resourceId);
+  res.render('delete', { resource }); 
+});
+
+app.post('/create', (req, res) => {
+  const newResource = req.body; 
+  const resources = readData();
+  newResource.id = resources.length + 1;  
+  resources.push(newResource); 
+  saveData(resources); 
+  res.redirect('/'); 
+});
+
+
+app.post('/update/:id', (req, res) => {
+  const resourceId = req.params.id;
+  let resources = readData();
+  const resourceIndex = resources.findIndex(r => r.id == resourceId);
+  resources[resourceIndex] = { ...resources[resourceIndex], ...req.body };  
+  saveData(resources);  
+  res.redirect('/');  
+});
+
+// POST route for deleting a resource
+app.post('/delete/:id', (req, res) => {
+  const resourceId = req.params.id;
+  let resources = readData();
+  resources = resources.filter(r => r.id != resourceId);  
+  saveData(resources);  
+  res.redirect('/');  
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
